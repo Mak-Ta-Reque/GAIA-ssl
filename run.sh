a29e300a7b13a93bd47010f0d0c85d4531675d2d
Md Abdul Kadir <maktareq@gmail.com>
2022-05-23 11:57:03 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+srun \
+  --container-image=/netscratch/duynguyen/Docker-Image/vissl_A100.sqsh -v\
+  -p RTX6000 --ntasks=1 --gpus-per-task=2 --cpus-per-gpu=8 --mem-per-cpu=16G\
+  --container-mounts=/netscratch/software:/netscratch/software:ro,/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
+  --container-workdir="`pwd`" \
+  --time=04:00:00 \
+  start_code_server.sh
