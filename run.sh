0affa965ca9145b6dfaf2efcce3f13a2980720f5
Md Abdul Kadir <maktareq@gmail.com>
2022-05-23 12:26:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ srun \
   -p RTXA6000 --ntasks=1 --gpus-per-task=2 --cpus-per-gpu=8 --mem-per-cpu=16G\
   --container-mounts=/netscratch/software:/netscratch/software:ro,/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
   --container-workdir="`pwd`" \
-  --time=04:00:00 \
-  bash
+  --task-prolog=install.sh\
+  --time=04:00:00\
