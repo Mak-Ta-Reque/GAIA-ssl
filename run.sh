90b86db40de68f2632fd542736fa6d15395834dd
Md Abdul Kadir <maktareq@gmail.com>
2022-05-23 12:00:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 srun \
   --container-image=/netscratch/duynguyen/Docker-Image/vissl_A100.sqsh -v\
-  -p RTX6000 --ntasks=1 --gpus-per-task=2 --cpus-per-gpu=8 --mem-per-cpu=16G\
+  -p RTXA6000 --ntasks=1 --gpus-per-task=2 --cpus-per-gpu=8 --mem-per-cpu=16G\
   --container-mounts=/netscratch/software:/netscratch/software:ro,/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
   --container-workdir="`pwd`" \
   --time=04:00:00 \
